57487bfcc5090a767586f19bea71820397f3c9d6
Maitreya Badola <maitreya.badola@live.in>
2019-06-20 09:52:25 +0530

    first commit

---
+++ TableStorage/Model/Author.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.CosmosDB.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableStorage.Model
+{
+    class Author : TableEntity
+    {
+        public Author()
+        {
+
+        }
+
+        public Author(string genere, string publisher)
+        {
+            this.PartitionKey = publisher;
+            this.RowKey = genere;
+        }
+
+        public string Title { get; set; }
+
+        public Double Price { get; set; }
+
+        public int PublishedYear { get; set; }
+    }
+}
